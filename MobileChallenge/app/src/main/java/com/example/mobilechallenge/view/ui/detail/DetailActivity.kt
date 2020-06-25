d3411419278e9b4038da81e24ed61ec373252177
Márcio Junior <mlpfj@hotmail.com>
2020-06-25 15:33:01 -0300

    Criação da inteface (Layout) da tela de detalhes do game (DetailActivity).

--- MobileChallenge/app/src/main/java/com/example/mobilechallenge/view/ui/detail/DetailActivity.kt
+++ MobileChallenge/app/src/main/java/com/example/mobilechallenge/view/ui/detail/DetailActivity.kt
@@ -2,11 +2,23 @@ package com.example.mobilechallenge.view.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.mobilechallenge.R
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = ""
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home)
+            finish()
+        return super.onOptionsItemSelected(item)
     }
 }
