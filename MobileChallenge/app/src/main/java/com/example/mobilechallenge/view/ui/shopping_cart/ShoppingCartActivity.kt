5b2c7f991edfdddcf629ae3184a9bde91f6c76b4
Márcio Junior <mlpfj@hotmail.com>
2020-06-26 11:24:53 -0300

    Criando a tela de carinho de compras (ShoppingCartActivity).

---
+++ MobileChallenge/app/src/main/java/com/example/mobilechallenge/view/ui/shopping_cart/ShoppingCartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobilechallenge.view.ui.shopping_cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobilechallenge.R
+
+class ShoppingCartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_cart)
+    }
+}
